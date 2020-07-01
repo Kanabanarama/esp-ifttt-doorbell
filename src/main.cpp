40df4e295d31aae03c22cf0aed34773a9aaf3bbb
Kana <kanabanarama@googlemail.com>
2020-07-01 13:19:53 +0200

    Improved WPS button interfacing and added WPS reset

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,14 @@
 
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <EasyButton.h>
 
-#define sensorPin D1
-#define sensorPinInverted D2
+#define sensorPin D8
+#define sensorPinInverted D9
 #define wpsPin D3
+#define wpsLed LED_BUILTIN
+
+EasyButton wpsButton(wpsPin);
 
 const char* ifttt_event = "---YOUR IFTTT EVENT---";
 const char* ifttt_key = "---YOUR IFTT KEY---";
@@ -37,7 +41,8 @@ void checkIfCallHasToBeMade() {
   char url[32];
   sprintf(url, path, host, ifttt_key, ifttt_key);
 
-  http.begin(url);
+  WiFiClient client;
+  http.begin(client, url);
   http.addHeader("Content-Type", "text/plain");
 
   int httpCode = http.POST("Message from ESP8266");
@@ -51,7 +56,7 @@ void checkIfCallHasToBeMade() {
 
 bool startWpsConfiguration() {
   Serial.println("WPS-Konfiguration gestartet.");
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(wpsLed, LOW);
   bool wpsSuccess = WiFi.beginWPSConfig();
   if(wpsSuccess) {
       String newSSID = WiFi.SSID();
@@ -60,50 +65,75 @@ bool startWpsConfiguration() {
       } else {
         wpsSuccess = false;
       }
+  } else {
+    Serial.println("WPS-Konfiguration fehlgeschlagen.");
   }
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(wpsLed, HIGH);
+  Serial.println(wpsSuccess);
   return wpsSuccess;
 }
+
+bool eraseWpsConfiguration() {
+  Serial.println("Resetting WPS data.");
+  digitalWrite(wpsLed, LOW);
+  ESP.eraseConfig();
+  delay(1000);
+  digitalWrite(wpsLed, HIGH);
+  ESP.reset();
+
+  return true;
+}
+
+boolean buttonState = false;
+
+boolean buttonActive = false;
+
+long buttonTimer = 0;
+unsigned int longPressTime = 1000;
 
 void setup()
 {
     pinMode(wpsPin, INPUT_PULLUP);
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
+    pinMode(wpsLed, OUTPUT);
+    digitalWrite(wpsLed, HIGH);
     pinMode(sensorPin, INPUT);
     pinMode(sensorPinInverted, INPUT);
 
+    delay(1000);
     Serial.begin(9600);
     delay(1000);
+
+    Serial.println("Ready");
+
+    wpsButton.onPressed(startWpsConfiguration);
+    wpsButton.onPressedFor(3000, eraseWpsConfiguration);
 
     WiFi.mode(WIFI_STA);
 
     String wifiSSID = WiFi.SSID().c_str();
     String wifiPSK = WiFi.psk().c_str();
 
-    if(wifiSSID.length() > 0) {
-      WiFi.begin(wifiSSID, wifiPSK);
-      int retry = 0;
-      while ((WiFi.status() == WL_DISCONNECTED) && (retry < 10)) {
-        delay(500);
-        Serial.print(".");
-        retry++;
-      }
-
-      wl_status_t status = WiFi.status();
-      if(status == WL_CONNECTED) {
-        Serial.println("verbunden.");
-        Serial.printf("SSID: '%s'\n", WiFi.SSID().c_str());
-        Serial.printf("IP address: '%s'", WiFi.localIP().toString().c_str());
-      }
-    } else {
+    if(wifiSSID.length() <= 0) {
       Serial.println("Keine WiFi Verbindung möglich.");
       Serial.println("Bitte WPS Taste am Router und am Gerät drücken.");
+      return;
+    }
 
-      while(digitalRead(wpsPin)!=0) { yield(); }
-      if(!startWpsConfiguration()) {
-         Serial.println("WPS-Konfiguration fehlgeschlagen.");
-      }
+    Serial.printf("Verbindung mit '%s' wird hergestellt.\n", wifiSSID.c_str());
+    WiFi.begin(wifiSSID, wifiPSK);
+    WiFi.setAutoConnect(true);
+    int retry = 0;
+    while ((WiFi.status() == WL_DISCONNECTED) && (retry < 10)) {
+      delay(500);
+      Serial.print(".");
+      retry++;
+    }
+
+    wl_status_t status = WiFi.status();
+    if(status == WL_CONNECTED) {
+      Serial.println("verbunden.");
+      Serial.printf("SSID: '%s'\n", WiFi.SSID().c_str());
+      Serial.printf("IP address: '%s'", WiFi.localIP().toString().c_str());
     }
 
     attachInterrupt(digitalPinToInterrupt(sensorPin), ISRoutine, RISING);
@@ -111,6 +141,6 @@ void setup()
 }
 
 void loop() {
+  wpsButton.read();
   checkIfCallHasToBeMade();
-  delay(50);
 }
